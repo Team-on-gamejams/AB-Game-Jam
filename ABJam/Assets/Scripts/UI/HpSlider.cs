5d4fc1903ba65184df2924b69a9aa47d2149bb8f
Team <timonsol13@gmail.com>
2019-12-09 14:28:47 +0200

    Import assets

---
+++ ABJam/Assets/Scripts/UI/HpSlider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpSlider : MonoBehaviour {
+	[SerializeField] Slider[] sliders;
+
+	public void Init(float min, float max) {
+		foreach (var slider in sliders) {
+			slider.minValue = min;
+			slider.maxValue = max;
+			slider.value = max;
+		}
+	}
+
+	public void UpdateValue(float currFill) {
+		foreach (var slider in sliders) {
+			LeanTween.cancel(gameObject, false);
+			LeanTween.value(slider.value, currFill, 0.5f)
+				.setOnUpdate((float fill)=> {
+					slider.value = fill;
+				});
+		}
+	}
+}
---
+++ ABJam/Assets/Scripts/UI/ShowOnMouseOver.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ShowOnMouseOver : MonoBehaviour, IPointerEnterHandler {
+	static GameObject curr;
+
+	[SerializeField] GameObject go;
+
+	public void OnPointerEnter(PointerEventData eventData) {
+		curr?.SetActive(false);
+		(curr = go)?.SetActive(true);
+	}
+}
