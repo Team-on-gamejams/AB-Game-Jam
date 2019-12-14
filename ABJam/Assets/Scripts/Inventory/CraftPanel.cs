375f4177fa4639da2935d6f23e8ebb845293efa2
Team <timonsol13@gmail.com>
2019-12-14 16:27:19 +0200

    DragNDrop inventory

---
+++ ABJam/Assets/Scripts/Inventory/CraftPanel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class CraftPanel : MonoBehaviour, IDropHandler {
+	[NonSerialized] public List<Item> items;
+
+	private void Awake() {
+		items = new List<Item>();
+	}
+
+	public bool CanCraft(params Item[] items) {
+		return false;
+	}
+
+	public Item Craft(params Item[] items) {
+		return null;
+	}
+
+	public void OnDrop(PointerEventData eventData) {
+		Item.isDragCatch = true;
+		if(!items.Contains(eventData.selectedObject.GetComponent<Item>()))
+			items.Add(eventData.selectedObject.GetComponent<Item>());
+	}
+}
