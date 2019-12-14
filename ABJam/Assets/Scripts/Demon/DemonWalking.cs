b993f799f713c18908c1d8a3aa7f05764ec2ace2
Danylo Lahodniuk <45416328+Long-as-Python@users.noreply.github.com>
2019-12-14 23:35:26 +0200

    AI for Death and demon

---
+++ ABJam/Assets/Scripts/Demon/DemonWalking.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemonWalking : MonoBehaviour
+{
+    public float speed = 100f;
+    public Animator animator;
+    public Vector2 direction;
+    public bool flip;
+
+    Rigidbody2D rb2d;
+    Vector3 movement;
+
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+		movement = direction * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        direction = -direction;
+    }
+
+    private void FixedUpdate()
+    {
+        if (
+            (movement.x < 0 && transform.localScale.x > 0 && flip == true ) ||
+            (movement.x > 0 && transform.localScale.x < 0 && flip == true)
+            )
+        {
+            Vector3 rot = transform.localScale;
+            rot.x = -rot.x;
+            transform.localScale = rot;
+        }
+        animator.SetFloat("Forward", movement.y);
+        rb2d.MovePosition(transform.position + movement);
+    }
+}
