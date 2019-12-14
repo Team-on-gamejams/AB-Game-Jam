44ea20d47b948b4f4e305918ecef0383da53c968
Danylo Lahodniuk <45416328+Long-as-Python@users.noreply.github.com>
2019-12-14 16:53:33 +0200

    Arts, Animations, Controller for hero

---
+++ ABJam/Assets/Scripts/Hero_scripts/herowalking.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class herowalking : MonoBehaviour
+
+{
+    private Rigidbody2D rb2D;
+    float moveH = 0f, moveV = 0f;
+    public float speed = 10f;
+    private Rigidbody2D rb2d;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    //================================================
+    void Update()
+    {
+        
+        
+    }
+    void FixedUpdate()
+    {
+        
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        
+        
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Forward", moveVertical);
+
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        
+        rb2d.AddForce(movement * speed);
+    }
+}
