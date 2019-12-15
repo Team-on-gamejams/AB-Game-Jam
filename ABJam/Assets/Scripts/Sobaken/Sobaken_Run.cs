073dac4408c8aed4a4c3d135c2a6b62f4aea37d0
Danylo Lahodniuk <45416328+Long-as-Python@users.noreply.github.com>
2019-12-15 10:16:50 +0200

    Sobacken Is here

---
+++ ABJam/Assets/Scripts/Sobaken/Sobaken_Run.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sobaken_Run : MonoBehaviour
+{
+    public float speed = 0.5f;
+    public Animator animator;
+    public GameObject objectGetting;
+    Rigidbody2D rb2d;
+    Vector3 movement;
+    
+    void Start()
+    {
+            
+    }
+
+    
+    void Update()
+    {
+        movement = objectGetting.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.MovePosition(transform.position + movement);
+    }
+}
