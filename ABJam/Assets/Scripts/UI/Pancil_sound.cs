fc33f952c673ff24f6d4cde2da3fa0060288dc70
Danylo Lahodniuk <45416328+Long-as-Python@users.noreply.github.com>
2019-12-15 11:58:24 +0200

    Sounds

---
+++ ABJam/Assets/Scripts/UI/Pancil_sound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pancil_sound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource audioSource;
+    public AudioClip clip;
+    public float volume = 0.5f;
+
+    public void CrossSound()
+    {
+        audioSource.PlayOneShot(clip);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
